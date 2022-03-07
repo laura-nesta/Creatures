1074f41b74c725e7b9b648cca12d2984af015346
laura-nesta <99044194+laura-nesta@users.noreply.github.com>
2022-03-07 11:32:55 +0100

    maj phisique des ailes
    droite et gauche
    vol en hauteur
    bug quand les ailes se desynchroniqes

--- Assets/AileDroite.cs
+++ Assets/AileDroite.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class AileDroite : MonoBehaviour
 {
+    public GameObject maCreature;
     public float Vitesse = 100.0f;
     // Start is called before the first frame update
     void Start()
@@ -16,14 +17,23 @@ public class AileDroite : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(valRotaion == 46 || valRotaion == -46)
+                rotationScale = -rotationScale;
         //batement aile
         if(Input.GetKey("space")){
-            if(valRotaion == 46 || 
-                valRotaion == -46)
-                rotationScale = -rotationScale;
             transform.Rotate(-rotationScale,0,0);
-            //transform.Translate(0,0.50f,0);
             valRotaion += rotationScale;
+            if(rotationScale < 0){
+               maCreature.transform.Translate(0,0.50f,0); 
+               
+            }
+        }
+        if(Input.GetKey(KeyCode.LeftArrow)){
+            transform.Rotate(rotationScale,0,0);
+            valRotaion += rotationScale;
+            if(rotationScale > 0){
+                 maCreature.transform.Translate(0,0,0.25f);
+            }
         }
     }
 } 
--- Assets/AileGauche.cs
+++ Assets/AileGauche.cs
@@ -13,24 +13,27 @@ public class AileGauche : MonoBehaviour
 
     int rotationScale = 1;
     int valRotaion = 0;
-    int Vitesse = 10;
     // Update is called once per frame
     void Update()
     {
         //battement aile 
-        if(Input.GetKey("space")){
-            if(valRotaion == 46 || valRotaion == -46)
+        if(valRotaion == 46 || valRotaion == -46)
                 rotationScale = -rotationScale;
+        if(Input.GetKey("space")){
             transform.Rotate(rotationScale,0,0);
             valRotaion += rotationScale;
             if(rotationScale > 0){
                maCreature.transform.Translate(0,0.50f,0); 
                
             }
-            if(Input.GetKey(KeyCode.RightArrow)){
-                maCreature.transform.Translate(Vector3.right*Time.deltaTime*Vitesse);
-                
+        }
+        if(Input.GetKey(KeyCode.RightArrow)){
+            transform.Rotate(rotationScale,0,0);
+            valRotaion += rotationScale;
+            if(rotationScale > 0){
+                 maCreature.transform.Translate(0,0,-0.25f);
             }
         }
+    
     }
 }
