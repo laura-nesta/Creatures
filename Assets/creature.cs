6b032fcc1e67c093109d805f9f34a8c0cd497138
CAYUELA ANNA-LUDVINA p1907353 <anna-ludvina.cayuela@etu.univ-lyon1.fr>
2022-03-07 11:22:47 +0100

    humm

--- Assets/creature.cs
+++ Assets/creature.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class creature : MonoBehaviour
 {
+    int a;
     public float Vitesse = 100.0f;
     // Start is called before the first frame update
     void Start()
@@ -14,31 +15,29 @@ public class creature : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        /*
         if(Input.GetKey(KeyCode.UpArrow)){
             transform.Translate(Vector3.forward*Time.deltaTime*Vitesse);
         }
-
         if(Input.GetKey(KeyCode.DownArrow)){
             transform.Translate(Vector3.back*Time.deltaTime*Vitesse);
         }
-
         if(Input.GetKey(KeyCode.LeftArrow)){
             transform.Translate(Vector3.left*Time.deltaTime*Vitesse);
         }
-
         if(Input.GetKey(KeyCode.RightArrow)){
             transform.Translate(Vector3.right*Time.deltaTime*Vitesse);
         }
+        */
         /*
         if(Input.GetKeyDown(KeyCode.Space)){
             transform.Translate(Vector3.left*Time.deltaTime*Vitesse);
         }*/
         if (Input.GetKey("space"))
         {
-            transform.Translate(0,1f,0);
+                //transform.Translate(0,0.50f,0);
         }
         //appeusenteure (deso je sais pas l'ecrire x) )
        // transform.Translate(0,-0.1f,0);
-
     }
 }
