aabc7ac5b988db4ee4df52a93b96ff2276d23a41
laura-nesta <99044194+laura-nesta@users.noreply.github.com>
2022-03-13 21:41:50 +0100

    ajout physique queue
    ajout du physique de la queue (permet d'avancer)

---
+++ Assets/queue.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class queue : MonoBehaviour
+{
+    public GameObject maCreature;
+    int rotationScale = 1;
+    int valRotaion = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if((valRotaion >= 35 && rotationScale > 0) || 
+         (valRotaion <= -35 && rotationScale < 0))
+                rotationScale = -rotationScale;
+        if(Input.GetKey("space")){
+            transform.Rotate(0,0,rotationScale);
+            valRotaion += rotationScale;
+            if(rotationScale > 0){
+               maCreature.transform.Translate(0,0.25f,0); 
+               
+            }
+        }
+        if(Input.GetKey(KeyCode.UpArrow)){
+            transform.Rotate(0,0,(float)0.5*rotationScale);
+            valRotaion += rotationScale;
+            if(rotationScale > 0){
+                 maCreature.transform.Translate(0,0,0.25f);
+                 maCreature.transform.Translate(-0.5f,0,0);
+                 maCreature.transform.Rotate(0,0,-0.1f);
+            }
+        }
+    }
+}
