2080043d4b79978cc32d48f716b254815558e1b0
CAYUELA ANNA-LUDVINA p1907353 <anna-ludvina.cayuela@etu.univ-lyon1.fr>
2022-03-14 12:27:30 +0100

    ajout de caractéristiques ailes

--- Assets/Aile.cs
+++ Assets/Aile.cs
@@ -4,23 +4,46 @@ using UnityEngine;
 
 public class Aile : MonoBehaviour
 {
-    public float Vitesse; //rapide, lent
     public int rotationScale;
     public int valRotation;
+
+    public float Vitesse; //rapide, lent
     public int poids;
-    public int taille;
+    public int taille; 
+    public int largeur; // large, étroite 
+    public int envergure;
+
     public int amplitude;
     public int frequence;
+
     public int turbulence;
+    /*
+    La turbulence désigne l'état de l'écoulement d'un fluide, liquide ou gaz, dans lequel la vitesse présente en tout point un caractère tourbillonnaire 
+    [formule à ajouter]
+    */
     public int portance; 
-    // composante de la force subie par un corps en mvt dans un fluide qui s'exerce perpendiculairement à la direction du mvt
+    // composante de la force subie par un corps en mvt dans un fluide qui s'exerce perpendiculairement à la direction du mvt 
+    // [chercher formule]
     public int allongement; //élevé ou pas
+    /*
+    L'allongement d'une aile (noté λ {\displaystyle \lambda } \lambda (lambda), est égal au quotient de l'envergure par la corde moyenne. 
+    Pour une aile rectangulaire sans flèche, il est donc égal au quotient de l'envergure par la corde.
+    Pour une aile de forme quelconque, l'allongement est égal au carré de l'envergure divisé par la surface portante :
+    FORMULE = λ = b²/S
+    */
+
+    public int manoeuvrabilite; //plus ou moin forte
+    public int habilete; // adresse 
     public int cambrure;
-    public int largeur;
-    public int battements; // continus ou non
+
+    public bool échancrure;
+    public bool interstices;
+    
+    
+    public int battements; //continu ou non
     public int emargination; //pour la pression de l'air
-    public int typeVol; //plané lent
-    public int manoeuvrabilite;
+    public int typeVol; //plané, lent... => enum?
+    
     
 
     // Start is called before the first frame update
