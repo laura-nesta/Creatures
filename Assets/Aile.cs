cab130ac12622ebfe61a5b464373813a49a9c8e5
CAYUELA ANNA-LUDVINA p1907353 <anna-ludvina.cayuela@etu.univ-lyon1.fr>
2022-03-14 12:14:23 +0100

    ajout de caractéristiques ailes

--- Assets/Aile.cs
+++ Assets/Aile.cs
@@ -4,9 +4,25 @@ using UnityEngine;
 
 public class Aile : MonoBehaviour
 {
-    public float Vitesse;
+    public float Vitesse; //rapide, lent
     public int rotationScale;
     public int valRotation;
+    public int poids;
+    public int taille;
+    public int amplitude;
+    public int frequence;
+    public int turbulence;
+    public int portance; 
+    // composante de la force subie par un corps en mvt dans un fluide qui s'exerce perpendiculairement à la direction du mvt
+    public int allongement; //élevé ou pas
+    public int cambrure;
+    public int largeur;
+    public int battements; // continus ou non
+    public int emargination; //pour la pression de l'air
+    public int typeVol; //plané lent
+    public int manoeuvrabilite;
+    
+
     // Start is called before the first frame update
     void Start()
     {
