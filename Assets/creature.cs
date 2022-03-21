7ba01a6e07d9dd6b70495a3acd6e78e557660b29
ahmed-atanane <ahmed.atanane07@gmail.com>
2022-03-21 09:18:20 +0100

    Nouvel asset d'oiseau avec sources et textures+ Script Aile.cs

--- Assets/creature.cs
+++ Assets/creature.cs
@@ -2,47 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class creature : MonoBehaviour
+public class Creature : MonoBehaviour
 {
-    public float Vitesse_Marche = 100.0f;
-    public float Acceleration = 5.0f;
-    public float Vitesse_Vol = 70.0f;
-    //Animation animations;
-    // Start is called before the first frame update
-    void Start()
-    {
-        //animations= gameObject.GetComponent<Animation>();
-        gameObject.GetComponent<Renderer>().material.color = Color.red;
-    }
+  public float Vitesse;
+  public Aile [] Ailes = new Aile[2];
+  public int taille;
+  public int poids;
+  
+  // Start is called before the first frame update
+  void Start()
+  {   
+      Vitesse = 100.0f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        if(Input.GetKey(KeyCode.UpArrow)){
-            transform.Translate(Vector3.forward*Time.deltaTime*Vitesse_Marche);
-        }
+      gameObject.GetComponent<Renderer>().material.color = Color.red;
+      Ailes[0].GetComponent<Renderer>().material.color = Color.green;
+      Ailes[1].GetComponent<Renderer>().material.color = Color.yellow;
+      
+      Ailes[0].rotationScale = 1;
+      Ailes[1].rotationScale = -1;
 
-        if(Input.GetKey(KeyCode.DownArrow)){
-            transform.Translate(Vector3.back*Time.deltaTime*Vitesse_Marche);
-        }
+  }
 
-        if(Input.GetKey(KeyCode.LeftArrow)){
-            transform.Translate(Vector3.left*Time.deltaTime*Vitesse_Marche);
-        }
+  // Update is called once per frame
+  void Update()
+  {
 
-        if(Input.GetKey(KeyCode.RightArrow)){
-            transform.Translate(Vector3.right*Time.deltaTime*Vitesse_Marche);
-        }
-        /*
-        if(Input.GetKeyDown(KeyCode.Space)){
-            transform.Translate(Vector3.left*Time.deltaTime*Vitesse);
-        }*/
-        if (Input.GetKey("space"))
-        {
-            transform.Translate(Vector3.up*Time.deltaTime*Vitesse_Vol);
-        }
-        //appeusenteure (deso je sais pas l'ecrire x) )
-       // transform.Translate(0,-0.1f,0);
-
-    }
+      
+  }
 }
