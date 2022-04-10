ca0c8d34be7319a53729b49d1108b4bcd1aafd90
CAYUELA ANNA-LUDVINA p1907353 <anna-ludvina.cayuela@etu.univ-lyon1.fr>
2022-04-10 17:32:50 +0200

    modif generation

--- Assets/Creature.cs
+++ Assets/Creature.cs
@@ -9,29 +9,52 @@ public class Creature : MonoBehaviour
     Vector3 target;
 
 //////////////////  CREATURE  /////////////////////
-    public AileDeFou [] tabAiles;
-    public Queue [] tabQueue;
-    public Oiseau corps;
+    AileDeFou [] tabAiles;
+    Queue [] tabQueue;
+    Oiseau corps;
+
+    public GameObject m_tabAiles;
+    public GameObject m_tabQueues;
+    GameObject m_Corps;
     GameObject m_Creature;
     Rigidbody r_Creature;
 
     public ADN genes;
     float aileg, ailed, queue, poids;
 
+    Vector3 [] v_aile;
+    Vector3 tmp_aile;
+
     public float timer = 0f;
     public float waitTime = 5f;
+
+    void Awake() {
+        tabAiles = new AileDeFou[nbAiles];
+        tabQueue = new Queue[nbQueues];
+        v_aile = new Vector3[nbAiles];
+        tmp_aile = new Vector3();
+    
+        
+        //m_tabAiles = new GameObject;
+        //m_tabQueues = new GameObject[nbQueues];
+        genes = new ADN(); 
+        
+        r_Creature = GetComponent<Rigidbody>();
+
+        for(int i=0; i<nbAiles; i++) {
+            tabAiles[i] = m_tabAiles.GetComponent<AileDeFou>();
+        }
+
+        for(int i=0; i<nbQueues; i++) {
+            tabQueue[i] = m_tabQueues.GetComponent<Queue>();
+        }
+        
+    }
 
 
     // Start is called before the first frame update
     void Start()
     {
-        tabAiles = new AileDeFou[nbAiles];
-        tabQueue = new Queue[nbQueues];
-        //genes = m_Creature.AddComponent<ADN>();
-        genes = new ADN(); 
-        
-        r_Creature = GetComponent<Rigidbody>();
-    
         // INITIALISATION DES COMPOSANTS DE LA CREATURE (avec les gènes)
        
         aileg = genes.getAileG();
@@ -40,23 +63,26 @@ public class Creature : MonoBehaviour
         poids = genes.getPoids();
 
         r_Creature.mass = poids;
-/*
+
         for(int i=0; i<nbAiles; i++){
+            v_aile[i] = tabAiles[i].getAile().transform.localScale;
+            tmp_aile = v_aile[i];
+            
             if(tabAiles[i].isAile_G){
-                tabAiles[i].getAile().transform.localScale *= aileg;
+                v_aile[i] = new Vector3(tmp_aile.x * aileg, tmp_aile.y * aileg, tmp_aile.z * aileg);
             }
             else{
-                tabAiles[i].getAile().transform.localScale *= ailed;
+                v_aile[i] *= ailed;
             }
         }
-
+        
         for(int i=0; i<nbQueues; i++){
             tabQueue[i].transform.localScale *= queue;
-        } */
+        } 
     }
 
     void Update() {
-        Debug.Log(ailed);
+        
     }
 
 
