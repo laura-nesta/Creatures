05823062f7d256d628a49d53f515b68b217801c9
ahmed-atanane <ahmed.atanane07@gmail.com>
2022-05-02 10:56:15 +0200

    Graphe

--- Assets/Scripts/Graphe.cs
+++ Assets/Scripts/Graphe.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+using System.Linq;
+using System.Text;
+using System.IO;
+
 public class Graphe: MonoBehaviour
 {
     public Evolution evolution;
@@ -11,17 +15,29 @@ public class Graphe: MonoBehaviour
     //Sprite: objet 2D pour afficher l'élément graphique du point
     [SerializeField] private Sprite dotsprite; //SerializeField permet de forcer à modifier la donnée privée.
 
+    public static string[] lignes;
+    private int nbvaleurs;
+    public float[] valeurs;
+
+	void Start ()
+	{
+		string fileName = "ScoresGraphe.txt";
+		lignes = File.ReadAllLines(fileName);
+
+		Debug.Log("Génération initiale: score: " + lignes[0] + " " + ", Génération finale: score: " + lignes[21]);
+
+        nbvaleurs=22;
+        valeurs=new float[nbvaleurs];
+        for(int i=0; i<nbvaleurs; i++)
+        {
+            valeurs[i]=float.Parse(lignes[i]);
+        }
+        AfficherGraphe(valeurs);
+	}
+
     private void Awake()
     {
         graphcontainer=transform.Find("graphcontainer").GetComponent<RectTransform>();
-
-        float[] valeurs;
-        valeurs=new float[100];
-        for(int i=0; i<100; i++)
-        {
-            valeurs[i]=evolution.gettabScore(i);
-        }
-        AfficherGraphe(valeurs);
     }
 
     private void Dot(Vector2 position)
@@ -41,9 +57,9 @@ public class Graphe: MonoBehaviour
        float hauteur=graphcontainer.sizeDelta.y;
        float yMinimum=100f;
        float ecart=50f;
-       for(int i=0; i<100; i++)
+       for(int i=0; i<nbvaleurs; i++)
        {
-           float xPosition=i*ecart+ecart;
+           float xPosition=i*ecart;
            float yPosition= (valeurs[i]/yMinimum)*hauteur;
            Dot(new Vector2(xPosition, yPosition));
        }
